3c328158a0797e8a9fb72a5ec0ea6c7f2fdbee66
SamGod69 <sampannatamrakar@gmail.com>
2026-06-02 16:13:12 +0545

    Completed setup of pnpm-workspace.yaml and react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./App.css";
+import Home from "./pages/Home/Home";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+]);
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return <></>;
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	);
 }
 
 export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+	return (
+		<div className="home-page">
+			<section className="hero-section">
+				<div className="container">
+					<nav>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+					</nav>
+				</div>
+			</section>
+		</div>
+	);
+};
+
+export default Home;
