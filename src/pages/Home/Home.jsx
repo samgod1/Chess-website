f996476059397875fd90247f7604a9f659feb349
SamGod69 <sampannatamrakar@gmail.com>
2026-06-05 12:57:11 +0545

    Completed home page with animations

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,85 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+
 import "./Home.css";
 
+gsap.registerPlugin(SplitText);
+
 const Home = () => {
+	useGSAP(() => {
+		let split = SplitText.create(".hero-text", { type: "lines" });
+
+		const heroTl = gsap.timeline();
+
+		heroTl.from(".img-container", {
+			delay: 0.5,
+			autoAlpha: 0,
+			duration: 1.5,
+		});
+
+		heroTl.from(
+			".h-text",
+			{
+				stagger: 0.1,
+				yPercent: 100,
+				ease: "back.inOut",
+				duration: 1.5,
+			},
+			"-=1.5",
+		);
+
+		heroTl.from(
+			".cta",
+			{
+				autoAlpha: 0,
+				duration: 0.5,
+			},
+			"-=.5",
+		);
+	}, []);
+
 	return (
 		<div className="home-page">
 			<section className="hero-section">
-				<div className="container">
+				<div className="nav-container">
 					<nav>
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
+						<span className="logo">Logo</span>
+						<ul>
+							<li>
+								<Link to="/courses">Courses</Link>
+							</li>
+							<li>
+								<Link to="/puzzles">Puzzles</Link>
+							</li>
+							<li>
+								<Link to="/visualization">Visualization</Link>
+							</li>
+							<button>Let's go</button>
+						</ul>
 					</nav>
 				</div>
+
+				<div className="container">
+					<div className="main-content">
+						<div className="hero-text">
+							<span className="hide-text">
+								<p className="h-text">LEARNING</p>
+							</span>
+							<span className="hide-text">
+								<p className="h-text">CHESS IS FUN</p>
+							</span>
+						</div>
+
+						<button className="cta">Let's go</button>
+					</div>
+					<div className="img-container">
+						<img src="/images/hero-img.png" alt="image" />
+					</div>
+				</div>
+				<div className="gradient"></div>
 			</section>
 		</div>
 	);
