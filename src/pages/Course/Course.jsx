67adf8eece3667a7225261282a17c03adf79e57a
SamGod69 <sampannatamrakar@gmail.com>
2026-06-29 17:00:55 +0545

    Complete info element design in course page

--- src/pages/Course/Course.jsx
+++ src/pages/Course/Course.jsx
@@ -7,10 +7,13 @@ import { Accordion, ProgressBar } from "../../components/index.js";
 import "./Course.css";
 
 const Course = () => {
+    const [completed, setCompleted] = useState(0);
+    const [total, setTotal] = useState(10);
+
     return (
         <div className="course-page">
             <div className="banner">This is a banner</div>
-            <div className="wrapper">
+            <div className="main-content">
                 <div className="course">
                     <Accordion
                         text={"Beginner"}
@@ -27,23 +30,70 @@ const Course = () => {
                 </div>
                 <div className="info">
                     <div className="progress-container">
-                        <span>Progress</span>
-                        <ProgressBar total={10} completed={0} />
-                        <div className="progress-count"></div>
+                        <h2>Progress</h2>
+                        <div className="progress-info">
+                            <ProgressBar total={total} completed={completed} />
+                            <div className="progress-count">
+                                {completed}/{total}
+                            </div>
+                        </div>
                     </div>
                     <div className="learning-info">
-                        <span>What you'll learn</span>
-                        <div className="beginner">
-                            <span>Beginner</span>
-                            <ul></ul>
+                        <h2>What you'll learn</h2>
+                        <div className="levels-info">
+                            <div className="beginner">
+                                <h3>Beginner</h3>
+                                <ul>
+                                    <li>How to move pieces</li>
+                                    <li>How to checkmate</li>
+                                    <li>Rules of chess</li>
+                                </ul>
+                            </div>
+                            <div className="intermediate">
+                                <h3>Intermediate</h3>
+                                <ul>
+                                    <li>How to move pieces</li>
+                                    <li>How to checkmate</li>
+                                    <li>Rules of chess</li>
+                                </ul>
+                            </div>
+                            <div className="professional">
+                                <h3>Professional</h3>
+                                <ul>
+                                    <li>How to move pieces</li>
+                                    <li>How to checkmate</li>
+                                    <li>Rules of chess</li>
+                                </ul>
+                            </div>
                         </div>
-                        <div className="intermediate">
-                            <span>Intermediate</span>
-                            <ul></ul>
-                        </div>
-                        <div className="professional">
-                            <span>Professional</span>
-                            <ul></ul>
+                    </div>
+                    <div className="badges">
+                        <h2>Earned badges</h2>
+                        <div className="earned">
+                            <div className="professional-badge">
+                                <img
+                                    src="/images/gold-medal.png"
+                                    alt="gold-medal"
+                                    width={40}
+                                    height={40}
+                                />
+                            </div>
+                            <div className="intermediate-badge">
+                                <img
+                                    src="/images/silver-medal.png"
+                                    alt="silver-medal"
+                                    width={40}
+                                    height={40}
+                                />
+                            </div>
+                            <div className="beginner-badge">
+                                <img
+                                    src="/images/bronze-medal.png"
+                                    alt="bronze-medal"
+                                    width={40}
+                                    height={40}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
