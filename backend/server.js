dc30cbb2ba1c1eb1e764dbfe228dbde1be4dfc63
SamGod69 <sampannatamrakar@gmail.com>
2026-07-10 20:17:08 +0545

    Setup backend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+    cors({
+        origin: "http:localhost:5173",
+    }),
+);
+
+app.get("/", (req, res) => {
+    return res.send("hello world");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port " + process.env.PORT);
+});
