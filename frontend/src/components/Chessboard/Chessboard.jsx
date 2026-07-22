4f1f6b26d4ec1306c4747cdf72152ed1a5d793ed
SamGod69 <sampannatamrakar@gmail.com>
2026-07-22 18:15:17 +0545

    Add feature of generating random squares and displaying them

--- frontend/src/components/Chessboard/Chessboard.jsx
+++ frontend/src/components/Chessboard/Chessboard.jsx
@@ -1,12 +1,44 @@
+import { useState, useRef } from "react";
+
 import "./Chessboard.css";
 
 const Chessboard = () => {
     const alphabets = ["a", "b", "c", "d", "e", "f", "g", "h"];
     const numbers = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
+    const [started, setStarted] = useState(false); //Vision practice
+    const [randomSquare, setRandomSquare] = useState(null);
+    const [color, setColor] = useState("white");
+
+    function generateRandomChessSquare() {
+        const firstRandomNumber = Math.floor(Math.random() * 8);
+        const secondRandomNumber = Math.floor(Math.random() * 8);
+
+        const randomSquare =
+            alphabets[firstRandomNumber] + numbers[secondRandomNumber];
+
+        setRandomSquare(randomSquare);
+    }
+
+    useState(() => {
+        if (started) {
+            generateRandomChessSquare();
+        }
+    }, [started]);
+
     return (
         <div className="chessboard-container">
-            <div className="chessboard white">
+            {started && (
+                <div className="randomSquareDisplay">{randomSquare}</div>
+            )}
+            <div
+                className="chessboard white"
+                onClick={() => {
+                    if (started) {
+                        generateRandomChessSquare();
+                    }
+                }}
+            >
                 {numbers.map((number, i) => {
                     //Alternating ranks
                     return i % 2 == 0 ? (
