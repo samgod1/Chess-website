ea2ad219c54b6ee17dac4d89809fe3167f47ee98
SamGod69 <sampannatamrakar@gmail.com>
2026-08-10 09:06:03 +0545

    Add redirection to signup page in puzzle and vision page if user isn't logged in

--- frontend/src/pages/Puzzles/Puzzles.jsx
+++ frontend/src/pages/Puzzles/Puzzles.jsx
@@ -1,12 +1,24 @@
 import { useEffect, useContext } from "react";
+import { useNavigate } from "react-router";
+import toast from "react-hot-toast";
 
 import "./Puzzles.css";
 import { Chessboard } from "../../components";
 import PuzzlesSidebar from "./components/PuzzlesSidebar/PuzzlesSidebar";
-import { ChessboardContext } from "../../contexts";
+import { UserContext, ChessboardContext } from "../../contexts";
 
 const Puzzles = () => {
+    const navigate = useNavigate();
+
+    const { user, loading } = useContext(UserContext);
     const { setMode } = useContext(ChessboardContext);
+
+    useEffect(() => {
+        if (!loading && !user) {
+            navigate("/signup");
+            toast.error("You have to signup / login first");
+        }
+    }, [loading]);
 
     useEffect(() => {
         setMode("puzzles");
--- frontend/src/pages/Vision/Vision.jsx
+++ frontend/src/pages/Vision/Vision.jsx
@@ -1,12 +1,24 @@
 import { useEffect, useContext } from "react";
+import { useNavigate } from "react-router";
+import toast from "react-hot-toast";
 
 import "./Vision.css";
 import { Chessboard } from "../../components/index.js";
 import VisionSidebar from "./components/VisionSidebar/VisionSidebar.jsx";
-import { ChessboardContext } from "../../contexts/";
+import { UserContext, ChessboardContext } from "../../contexts/";
 
 const Vision = () => {
+    const navigate = useNavigate();
+
+    const { user, loading } = useContext(UserContext);
     const { setMode } = useContext(ChessboardContext);
+
+    useEffect(() => {
+        if (!loading && !user) {
+            navigate("/signup");
+            toast.error("You have to signup / login first");
+        }
+    }, [loading]);
 
     useEffect(() => {
         setMode("vision");
